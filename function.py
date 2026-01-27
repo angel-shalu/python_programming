f41441af7c1cf69f31116ba5a63ae59e6856264c
Shalini Kumari <159770762+angel-shalu@users.noreply.github.com>
2026-01-27 23:07:37 +0530

    completed

--- function.py
+++ function.py
@@ -1,74 +1,150 @@
-"""When the large program can be divided into sub programs and these sub programs is content a block of code that perform a specific task.
-These block of code are called functions.
+# """When the large program can be divided into sub programs and these sub programs is content a block of code that perform a specific task.
+# These block of code are called functions.
 
-Functions can be used again and agin.
+# Functions can be used again and agin.
 
-# ---------------------------------------------------------------------------------
-TYES OF FUNCTIONS
-1. Built-in functions / Pre-defined functions
-Built-in functions: jo python me pre-defined hoti hai jaise print(), input(), len() etc.
-These functions are already declared in python and we can use them directly.
+# # ---------------------------------------------------------------------------------
+# TYES OF FUNCTIONS
+# 1. Built-in functions / Pre-defined functions
+# Built-in functions: jo python me pre-defined hoti hai jaise print(), input(), len() etc.
+# These functions are already declared in python and we can use them directly.
 
-2. User-defined functions
-User-defined functions: jo hum khud banate hai apne program ke liye.
-    1. No return type and no argument / parameter
-    2. No return but argument / parameter
-    3. Return without argument / parameter
-    4. Return with argument / parameter
+# 2. User-defined functions
+# User-defined functions: jo hum khud banate hai apne program ke liye.
+#     1. No return type and no argument / parameter
+#     2. No return but argument / parameter
+#     3. Return without argument / parameter
+#     4. Return with argument / parameter
 
-3. Anonymous functions / Lambda functions
-Anonymous functions: jo bina naam ke hoti hai aur ek line me hi likhi jati hai.
+# 3. Anonymous functions / Lambda functions
+# Anonymous functions: jo bina naam ke hoti hai aur ek line me hi likhi jati hai.
 
-# ---------------------------------------------------------------------------------
-Syntax of function:
-def function_name():
-    statement1
-    statement2
+# # ---------------------------------------------------------------------------------
+# Syntax of function:
+# def function_name():
+#     statement1
+#     statement2
     
--------------------------------------------------------------------------------------
-Types of variables in function you can create when declaring function:
--------------------------------------------------------------------------------------
-1. Local variable: jo function ke andar declare hota hai aur sirf usi function ke andar use hota hai.
-    When we define the function inside the function then it is called local variable.
-    The scope of local variable is available only inside the function.
+# -------------------------------------------------------------------------------------
+# Types of variables in function you can create when declaring function:
+# -------------------------------------------------------------------------------------
+# 1. Local variable: jo function ke andar declare hota hai aur sirf usi function ke andar use hota hai.
+#     When we define the function inside the function then it is called local variable.
+#     The scope of local variable is available only inside the function.
 
-2. Global variable: jo function ke bahar declare hota hai aur pure program me kahi bhi use hota hai.
-    When we define the function outside the function then it is called local variable.
-    The scope of local variable is available inside the function as well as outside the function.
+# 2. Global variable: jo function ke bahar declare hota hai aur pure program me kahi bhi use hota hai.
+#     When we define the function outside the function then it is called local variable.
+#     The scope of local variable is available inside the function as well as outside the function.
     
-# ---------------------------------------------------------------------------------
- """
-# ------------------ NO RETURN TYPE AND NO ARGUMENT / PARAMETER ------------------
-def my_function():
-    print("Hello from a function")  
-my_function()          # function call
-
-# ---------------------------------------------------------
-# Local variable example
-def add():
-    a = 10
-    b = 20
-    c = a + b
-    print(a)
-    print("Addition =", c)
-add()                  # function call
+# # ---------------------------------------------------------------------------------
+#  """
+# # ------------------ NO RETURN TYPE AND NO ARGUMENT / PARAMETER ------------------
+# def my_function():
+#     print("Hello from a function")  
+# my_function()          # function call
 
 # # ---------------------------------------------------------
-# def addition():
-#     num1 = int(input("Enter first number: "))
-#     num2 = int(input("Enter second number: "))
-#     sum = num1 + num2
-#     print("Sum =", sum)
-# addition()             # function call
+# # Local variable example
+# def add():
+#     a = 10
+#     b = 20
+#     c = a + b
+#     print(a)
+#     print("Addition =", c)
+# add()                  # function call
 
-# ----------------------------------------------------------
-# Global variable example
-a = 50                 # global variable
-def add():
-    a = 10
-    b = 20
-    c = a + b
-    print(a)                  # prints local variable a
-    print("Addition =", c)
-add()                  # function call
-print("Value of a outside function:", a)              # prints global variable a
+# # # ---------------------------------------------------------
+# # def addition():
+# #     num1 = int(input("Enter first number: "))
+# #     num2 = int(input("Enter second number: "))
+# #     sum = num1 + num2
+# #     print("Sum =", sum)
+# # addition()                  # function call
+
+# # ----------------------------------------------------------
+# # Global variable example
+# a = 50                        # global variable
+# def add():
+#     a = 10
+#     b = 20
+#     c = a + b
+#     print(a)                  # prints local variable a
+#     print("Addition =", c)
+# add()                  # function call
+# print("Value of a outside function:", a)              # prints global variable a
+
+
+# no return with argument
+
+def user_login(x):
+    print("hii",x)
+    
+user_login(10)
+
+
+
+# return with argument 
+
+def user_login(x):
+    return x
+    
+y=user_login(10)
+y
+
+# --------------------------------
+def user_login(age):
+    if(age<18):
+        return False
+    else:
+        return True
+    
+x=int(input("enter your age"))
+var=user_login(x)
+if(var==True):
+    print("vote")
+    
+else:
+    print("not vote")
+    
+
+# ----------------------------------
+def user_login(x=5):
+    for i in range(1,x+1):
+        for i in range(1,i+1):
+            print(i,end=" ")
+        print() 
+        
+user_login()
+
+# -----------------------------
+def user_login(x=5):
+    for i in range(1,x+1):
+        for i in range(1,x+1):
+            print(i,end=" ")
+        print() 
+        
+user_login()
+
+# ---------------------------------
+def user_login(x=5):
+    for i in range(1,x+1):
+        for i in range(1,i+1):
+            print("*",end=" ")
+        print() 
+        
+user_login()
+
+# ----------------------------------------
+
+def user_login(x=5):
+    for i in range(1,x+1):
+        for j in range(1,x+1):
+            if(i==1 or i==5 or j==1 or j==5):
+                print("*",end=" ")
+                
+            else:
+                print(" ", end=" ")
+                
+        print()
+        
+user_login()
