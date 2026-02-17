2d9e7de64c8fc80dc82628265f42db99007f6a5a
Shalini Kumari <159770762+angel-shalu@users.noreply.github.com>
2026-02-17 23:31:09 +0530

    python code

--- PY MYSQL CONNECTOR/crud_operation_using_py_mysql.py
+++ PY MYSQL CONNECTOR/crud_operation_using_py_mysql.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Make a simple project using python & mysql, Make a simple ceud operation, CREATE, READ, UPDATE & DELETE using choice based.
 
 
@@ -16,62 +15,72 @@ class Database_Connection:
         self.mycursor = self.mydb_connection.cursor()
         print("Database connected successfully...")
         
+
+    # CREATE
     def RecordInsert(self, roll_number, name, branch, address):
         self.roll_number = roll_number
         self.name = name
         self.branch = branch
         self.address = address
         
-        sql = "Insert into student_details (name, roll_number, class, address) values(%s,%s,%s,%s)"
-        value = [self.roll_number, self.name, self.branch, self.address]
-        self.mycursor.execute(sql, value)
-        self.mydb.commit()
+        query = "Insert into student_details (roll_number, name, branch, address) values(%s,%s,%s,%s)"
+        values = [self.roll_number, self.name, self.branch, self.address]
+        self.mycursor.execute(query, values)
+        self.mydb_connection.commit()
         print("Record inserted successfully...")
+    
+    
+    # READ
+    def ReadRecord(self):
+        query = "SELECT * FROM student_details"
+        self.mycursor.execute(query)
+        records = self.mycursor.fetchall()
+        print("\nRoll | Name | Branch | Address")
+        for row in records:
+            print(row)
+  
+  
+    # UPDATE
+    def UpdateRecord(self):
+        roll_number = input("Enter roll number to update: ")
+        branch = input("Enter the new branch: ")
+        address = input("Enter the new address: ")
+        query = """
+            UPDATE student_details
+            SET branch = %s, address = %s
+            WHERE roll_number = %s
+            """
+        values = (branch, address, roll_number)
+        self.mycursor.execute(query, values)
+        self.mydb_connection.commit()
+        print("Record updated successfully")
         
         
+    # SEARCH
+    def SearchRecord(self):
+        roll_number = input("Enter roll number to search: ")
+        query = """
+            SELECT * FROM student_details
+            WHERE roll_number = %s
+            """
+        values = [roll_number]
+        self.mycursor.execute(query, values)
+        records = self.mycursor.fetchall()
+        count = self.mycursor.rowcount
+        if count>=1:
+            print(records)
+        else:
+            print("No data found...")
+        print("Record search successfully")
+      
         
-        
+    # DELETE   
+    def DeleteRecord(self):
+        roll_number = input("Enter roll number to delete: ")
+        query = "DELETE FROM student_details WHERE roll_number = %s"
+        values = [roll_number]
+        self.mycursor.execute(query, values)
+        self.mydb_connection.commit()
+        print("Record deleted successfully")
         
 
-        
-Obj = Database_Connection()
-        
-=======
-# Make a simple project using python & mysql, Make a simple ceud operation, CREATE, READ, UPDATE & DELETE using choice based.
-
-
-import mysql.connector
-
-class Database_Connection:
-    def __init__(self):
-        self.mydb_connection = mysql.connector.connect(
-            host = "localhost",
-            user = "root",
-            password = "2005",
-            database = "student"
-        )
-        
-        self.mycursor = self.mydb_connection.cursor()
-        print("Database connected successfully...")
-        
-    def RecordInsert(self, roll_number, name, branch, address):
-        self.roll_number = roll_number
-        self.name = name
-        self.branch = branch
-        self.address = address
-        
-        sql = "Insert into student_details (name, roll_number, class, address) values(%s,%s,%s,%s)"
-        value = [self.roll_number, self.name, self.branch, self.address]
-        self.mycursor.execute(sql, value)
-        self.mydb.commit()
-        print("Record inserted successfully...")
-        
-        
-        
-        
-        
-
-        
-Obj = Database_Connection()
-        
->>>>>>> edc38913634259038058ced79e7e8598d76e3cae
--- PY MYSQL CONNECTOR/project.py
+++ PY MYSQL CONNECTOR/project.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import crud_operation_using_py_mysql
 obj = crud_operation_using_py_mysql.Database_Connection()
 
@@ -23,57 +22,21 @@ while True:
         obj.RecordInsert(roll_number, name, branch, address)
 
     elif choice == 2:
-        print("Read")
+        obj.ReadRecord()
 
     elif choice == 3:
-        print("Update")
+        obj.UpdateRecord()
 
     elif choice == 4:
-        print("Delete")
-
+        obj.SearchRecord()
+        
     elif choice == 5:
+        obj.DeleteRecord()
+
+    elif choice == 6:
         print("Program closed...")
         break
 
     else:
-        print("Wrong choice..")
-=======
-import crud_operation_using_py_mysql
-obj = crud_operation_using_py_mysql.Database_Connection()
-
-while True:
-    print("""
-    Press 1 for add data
-    Press 2 for read the data 
-    Press 3 for update the data 
-    Press 4 for delete the data 
-    Press 5 for exit
-    """)
-
-    choice = int(input("Enter your Choice: "))
-
-    if choice == 1:
-        print("Insert")
-        roll_number = input("Enter the roll_number: ")
-        name = input("Enter the name: ")
-        branch = input("Enter the branch: ")
-        address = input("Enter the address: ")
-
-        obj.RecordInsert(roll_number, name, branch, address)
-
-    elif choice == 2:
-        print("Read")
-
-    elif choice == 3:
-        print("Update")
-
-    elif choice == 4:
-        print("Delete")
-
-    elif choice == 5:
-        print("Program closed...")
-        break
-
-    else:
-        print("Wrong choice..")
->>>>>>> edc38913634259038058ced79e7e8598d76e3cae
+        print("Invalid choice")
+        
