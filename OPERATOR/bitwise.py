43859519c143133a856491543e6dcc8db3e3474f
Shalini Kumari <159770762+angel-shalu@users.noreply.github.com>
2026-02-18 20:48:24 +0530

    operator

--- OPERATOR/bitwise.py
+++ OPERATOR/bitwise.py
@@ -32,3 +32,44 @@ print(c)
 
 
 # ----------------------------------------------------------
+# BITWISE NOT OPERATOR
+# It is a unary operator and it is also known as complement operator    
+# -----------------------------------------------------------
+a = 10   #00001010
+b = ~a   #11110101
+print(b)
+
+c = ~b   #00001010
+print(c)
+
+
+
+# -----------------------------------------------------------
+# BITWISE LEFT SHIFT OPERATOR
+# It shifts the bits of the number to the left by specified number of positions and fills the vacated bits with 0
+# -----------------------------------------------------------
+a = 10      #00001010
+b = a << 1  #00010100    
+print(b)
+
+c = a << 2  #00101000
+print(c)
+
+x = c << 3  #01010000
+print(x)
+
+
+# -----------------------------------------------------------
+# BITWISE RIGHT SHIFT OPERATOR
+# It shifts the bits of the number to the right by specified number of positions and fills the vacated bits with 0
+# -----------------------------------------------------------
+a = 10      #00001010
+b = a >> 1  #00000101   
+print(b)
+
+c = a >> 2  #00000010
+print(c)
+
+x = a >> 3  #00000001
+print(x)
+
