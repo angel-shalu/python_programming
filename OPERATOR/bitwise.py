27f34927adfcd59f66f35e21cbf7c156e7fa79ac
angel-shalu <shalinikumari8789@gamil.com>
2026-02-18 12:06:04 +0530

    python code

---
+++ OPERATOR/bitwise.py
@@ -0,0 +1,34 @@
+"""BIWISE OPERATOR
+
+    """
+# ------------------------------------------------------
+# BITWISE AND OPERATOR
+# 
+
+   
+a = 10     #00001010
+b = 11     #00001011
+c = a & b  #00001010
+print(c)
+
+# -------------------------------------------------------
+# BITWISE OR OPERATIOR
+# If both bits are 0 then result should be 0 otherwise 1
+
+a = 10   #00001010
+b = 11   #00001011
+c = a|b  #00001011
+print(c)
+
+# -----------------------------------------------------------
+# BIWISE XOR OPERATOR
+# If both the bits will be same then result will be 0 otherwise 1
+
+
+a = 10   #00001010
+b = 11   #00001011
+c = a^b  #00000001
+print(c)
+
+
+# ----------------------------------------------------------
